4909aa3bb2907b957ef1a95b893c2d930115f756
iadaria <jadarya@mail.ru>
2020-08-03 09:03:27 +0900

    Updates

---
+++ precache-manifest.1b6288691c86f9b86689b92029252ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a539cd4da89a23d92b6cf709162aac8",
+    "url": "/github.io/think-piece/index.html"
+  },
+  {
+    "revision": "eeee09de4b95025cfa9e",
+    "url": "/github.io/think-piece/static/css/main.b167db64.chunk.css"
+  },
+  {
+    "revision": "1c49ac24033163e10883",
+    "url": "/github.io/think-piece/static/js/2.43b64174.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/github.io/think-piece/static/js/2.43b64174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeee09de4b95025cfa9e",
+    "url": "/github.io/think-piece/static/js/main.f63552d9.chunk.js"
+  },
+  {
+    "revision": "352f5b008dd7372617a4",
+    "url": "/github.io/think-piece/static/js/runtime-main.684e351f.js"
+  }
+]);
